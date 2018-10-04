14db5400b327c64b06ada5c4d24f71c31ddf747d
CrSjimo <crs_16423@outlook.com>
2018-10-04 13:25:58 +0800

    bug fix

--- js/2jfhs.js
+++ js/2jfhs.js
@@ -2,7 +2,7 @@ level=2;
 var next="3ozur";
 var tips="Ctrl+C\nCtrl+V";
 $("#next").click(function(){
-	if($("#answer").val().=="ihsvyudfabhnasubdc"){
+	if($("#answer").val()=="ihsvyudfabhnasubdc"){
 		location.pathname="/game/"+next+".html";
 	}else{
 		global.notice("Incorrect!",true);
